17691a3b78fc45091923f796522df9562542dac9
Shivam Kumar <shivamkumar271855@gmail.com>
2023-01-20 21:57:57 +0530

    Analytic template

---
+++ script.js
@@ -0,0 +1,8 @@
+let navbarMobile = ()=>{
+    if(document.querySelector('.boxMobile').style.display === 'block'){
+        document.querySelector('.boxMobile').style.display = 'none'
+    }
+    else{
+        document.querySelector('.boxMobile').style.display = 'block'
+    }
+}
